ea045fe18125f2234198a604a20a83dcd77bde0b
Austin <tashoecraft@gmail.com>
2015-12-05 19:32:59 -0500

    start of refactor

---
+++ browser/js/sandbox/sandbox.controller.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Austin on 12/2/15.
+ */
+app.controller('SandboxController', ($scope) => {
+
+});
---
+++ browser/js/sandbox/sandbox.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Austin on 12/2/15.
+ */
+app.config(($stateProvider) => {
+    $stateProvider.state('sandbox', {
+        url: '/sandbox',
+        templateUrl: 'js/sandbox/sandbox.html',
+        controller: 'SandBoxController'
+
+    });
+});
